3e2c0e24bf3e881d6e864f74c3896928eaf79545
Franck MEYER <meyerfranckpro@gmail.com>
2020-05-25 21:27:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,8 +13,9 @@ import pandas as pd
 import os, json
 
 def imShowScale(im,scale=10):
+  import cv2
   imResize=cv2.resize(im,(int(im.shape[1]/scale),int(im.shape[0]/scale)))
-  #cv2_imshow(imResize)
+  cv2_imshow(imResize)
 
 def prepareExcelFile(rootPath,fileName):
   def add_name_and_commonPath_columns(data):
